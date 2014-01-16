8dc95b6835c7c68736cd250386f17fd18cc0cb29
Knut Nesheim <knutin@gmail.com>
2014-01-16 18:38:41 +0100

    Better tests.

--- test/hyper_test.erl
+++ test/hyper_test.erl
@@ -17,6 +17,7 @@ hyper_test_() ->
       ?_test(union_t()),
       ?_test(small_big_union_t()),
       ?_test(intersect_card_t()),
+      ?_test(bad_serialization_t()),
       {timeout, 600, fun () -> ?assert(proper:quickcheck(prop_set(),
                                                          [{max_size, 1000},
                                                           {numtests, 1000},
@@ -24,8 +25,7 @@ hyper_test_() ->
       {timeout, 600, fun () -> ?assert(proper:quickcheck(prop_serialize(),
                                                          [{max_size, 1000},
                                                           {numtests, 1000},
-                                                          {to_file, user}])) end},
-      ?_test(bad_serialization_t())
+                                                          {to_file, user}])) end}
      ]}.
 
 basic_t() ->
@@ -36,13 +36,8 @@ basic_t() ->
 
 
 serialization_t() ->
-    Mod = hyper_binary_rle,
+    Mod = hyper_binary,
     Hyper = hyper:compact(hyper:insert_many(generate_unique(10), hyper:new(5, Mod))),
-
-    {Mod, L} = Hyper#hyper.registers,
-    {Mod, R} = (hyper:compact(
-                  hyper:from_json(
-                    hyper:to_json(Hyper), Mod)))#hyper.registers,
 
     ?assertEqual(trunc(hyper:card(Hyper)),
                  trunc(
@@ -50,8 +45,8 @@ serialization_t() ->
                      hyper:from_json(
                        hyper:to_json(Hyper), Mod)))),
     ?assertEqual(Hyper#hyper.p, (hyper:from_json(
-                                   hyper:to_json(Hyper), Mod))#hyper.p),
-    ?assertEqual(L, R).
+                                   hyper:to_json(Hyper), Mod))#hyper.p).
+
 
 
 backend_t() ->
@@ -63,20 +58,11 @@ backend_t() ->
     Array  = hyper:compact(hyper:insert_many(Values, hyper:new(P, hyper_array))),
     Bisect = hyper:compact(hyper:insert_many(Values, hyper:new(P, hyper_bisect))),
     Binary = hyper:compact(hyper:insert_many(Values, hyper:new(P, hyper_binary))),
-    Rle    = hyper:compact(hyper:insert_many(Values, hyper:new(P, hyper_binary_rle))),
 
     {hyper_gb    , GbRegisters}     = Gb#hyper.registers,
     {hyper_array , ArrayRegisters}  = Array#hyper.registers,
     {hyper_bisect, BisectRegisters} = Bisect#hyper.registers,
     {hyper_binary, BinaryRegisters} = Binary#hyper.registers,
-    {hyper_binary_rle, RleRegisters} = Rle#hyper.registers,
-
-    %% error_logger:info_msg("Gb:     ~p~nArray:  ~p~nBisect: ~p~nBinary: ~p~n",
-    %%                       [hyper_gb:encode_registers(GbRegisters),
-    %%                        hyper_array:encode_registers(ArrayRegisters),
-    %%                        hyper_bisect:encode_registers(BisectRegisters),
-    %%                        hyper_binary:encode_registers(BinaryRegisters)]),
-
 
     ExpectedRegisters = lists:foldl(
                           fun (Value, Registers) ->
@@ -107,30 +93,22 @@ backend_t() ->
     ?assertEqual(ExpectedBytes, hyper_array:encode_registers(ArrayRegisters)),
     ?assertEqual(ExpectedBytes, hyper_bisect:encode_registers(BisectRegisters)),
     ?assertEqual(ExpectedBytes, hyper_binary:encode_registers(BinaryRegisters)),
-    ?assertEqual(ExpectedBytes, hyper_binary_rle:encode_registers(RleRegisters)),
-
-    %% error_logger:info_msg("~p~n~p~n", [hyper:from_json(
-    %%                                      hyper:to_json(Array),
-    %%                                      hyper_binary_rle),
-    %%                                   Rle]),
 
     ?assertEqual(hyper:card(Gb),
                  hyper:card(hyper:from_json(hyper:to_json(Array), hyper_gb))),
     ?assertEqual(Array, hyper:from_json(hyper:to_json(Array), hyper_array)),
     ?assertEqual(Array, hyper:from_json(hyper:to_json(Bisect), hyper_array)),
     ?assertEqual(Bisect, hyper:from_json(hyper:to_json(Array), hyper_bisect)),
-    ?assertEqual(Rle, hyper:from_json(hyper:to_json(Array), hyper_binary_rle)),
 
 
     ?assertEqual(hyper:to_json(Gb), hyper:to_json(Array)),
     ?assertEqual(hyper:to_json(Gb), hyper:to_json(Bisect)),
     ?assertEqual(hyper:to_json(Gb), hyper:to_json(Binary)),
-    ?assertEqual(hyper:to_json(Gb), hyper:to_json(Rle)),
 
     ?assertEqual(hyper:card(Gb), hyper:card(Array)),
     ?assertEqual(hyper:card(Gb), hyper:card(Bisect)),
-    ?assertEqual(hyper:card(Gb), hyper:card(Binary)),
-    ?assertEqual(hyper:card(Gb), hyper:card(Rle)).
+    ?assertEqual(hyper:card(Gb), hyper:card(Binary)).
+
 
 
 
@@ -347,7 +325,7 @@ bad_serialization_t() ->
 %%
 
 backends() ->
-    [hyper_gb, hyper_array, hyper_bisect, hyper_binary, hyper_binary_rle].
+    [hyper_gb, hyper_array, hyper_bisect, hyper_binary].
 
 
 gen_values() ->
@@ -413,14 +391,18 @@ prop_set() ->
 
 prop_serialize() ->
     ?FORALL(
-       {Mod, P}, {oneof(backends()), choose(4, 16)},
+       {LeftMod, RightMod, P}, {oneof(backends()),
+                                oneof(backends()),
+                                choose(4, 16)},
        ?FORALL(
           Values, gen_values(),
           begin
-              H = hyper:compact(hyper:insert_many(Values, hyper:new(P, Mod))),
-              H =:= hyper:compact(
-                      hyper:from_json(
-                        hyper:to_json(H), Mod))
+              Left = hyper:compact(
+                       hyper:insert_many(Values, hyper:new(P, LeftMod))),
+              Right = hyper:compact(
+                        hyper:insert_many(Values, hyper:new(P, RightMod))),
+
+              hyper:to_json(Left) =:= hyper:to_json(Right)
           end)).
 
 %%
